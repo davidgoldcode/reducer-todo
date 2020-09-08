cf28a841f41548f3688f0302d763084d2a622c75
David Gold <davidgold@hey.com>
2020-09-08 18:36:06 -0400

    clear completed in there

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,11 +1,16 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer, useState, useEffect } from 'react';
 import {toDoReducer, initialState} from '../reducers/index'
 
 const Todo = () => {
-    const [newTaskText, setNewTaskText] = useState('')
     const [state, dispatch] = useReducer(toDoReducer, initialState);
+    const [newTaskText, setNewTaskText] = useState('');
+
+    useEffect(() => {
+        console.log(state);
+    }, []);
 
     const handleChanges = (event) => {
+        event.preventDefault();
         setNewTaskText(event.target.value);
     }    
 
@@ -17,19 +22,27 @@ const Todo = () => {
         <div>
             <ul>To do list</ul>
                 {state.map((item) => (
-                    <ol onClick={() => dispatch({type: "TOGGLE_TASK"})}>{item.task}</ol>
+                    <ol onClick={() => dispatch({type: "TOGGLE_TASK", payload: item.id})}>{item.task}</ol>
                 ))}
             <div>
                 <form>
                     <input
                     type='text'
-                    name='newTask'
+                    name='newTaskText'
                     value={newTaskText}
                     onChange={handleChanges}
                     />
-                    <button onClick={() => {
-                        dispatch({type: "ADD_TASK", payload: newTaskText})
+                    <div>
+                        <button onClick={() => {
+                    dispatch({type: "ADD_TASK", payload: newTaskText})
                     }}>Add Task</button>
+                    </div>
+                    <div>
+                        <button onClick={() => {
+                            dispatch({type: "CLEAR_COMPLETED"})
+                        }}>Clear Completed</button>
+                    </div>
+                    
                 </form>
             </div>
         </div>
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Todo from '../Components/Todo';
 
 export const initialState = [{
     task: 'meal prep',
@@ -8,12 +9,12 @@ export const initialState = [{
 {
     task: 'clean room',
     completed: false,
-    id: 23498234089, 
+    id: 33245244, 
 }, 
 {
     task: 'do lambda hw',
     completed: false,
-    id: 29385982345, 
+    id: 23525424, 
 }
 ]
 
@@ -21,17 +22,18 @@ export function toDoReducer(state, action) {
     console.log(state, action);
     switch (action.type) {
         case "ADD_TASK":
-            return [...state, {
+            const newTodo = {
                 id: new Date(),
                 task: action.payload,
                 completed: false,
-            }]
+            }
+            return [...state, newTodo]
         case "TOGGLE_TASK":
-            return [...state, {
-                id: new Date(),
-                task: action.payload,
-                completed: false,
-            }]        
+            console.log(action.payload);
+            return state.map((item) => (item.id === action.payload ? {...item, completed: !item.completed } : item ))
+        case "CLEAR_COMPLETED":
+            console.log(action.payload);
+            return state.filter((item) => item.completed === false);
         default:
             return state;
     }
